cfd0c0371c7c69d007ce4ab61718ebbf9da80408
mwasanje <noaheben@gmail.com>
2022-11-05 14:12:56 -0500

    Separeted logic into different files.

--- main.lua
+++ main.lua
@@ -1,35 +1,10 @@
 Object = require "libraries/classic"
+Gamestate = require "libraries/gamestate"
 require "button"
-buttons = {}
+require "menu"
+require 'levelOne'
 
 function love.load()
-  ww = love.graphics.getWidth()
-  wh = love.graphics.getHeight()
-  button = love.graphics.newImage('images/button1.png')
-  bg = love.graphics.newImage('images/bg.png')
-  logo = love.graphics.newImage('images/logo.png')
-  button_1 = Button(ww/2 - button:getWidth()/2, 300, "Play")
-  table.insert(buttons, button_1)
-  button_2 = Button(ww/2 - button:getWidth()/2, 400, "Quit")
-  table.insert(buttons, button_2)
-end
-
-function love.update(dt)
-  for i, button in ipairs(buttons) do
-    button:update(dt)
-  end
-end
-
-function love.draw()
-  love.graphics.draw(bg, 0, 0)
-  love.graphics.draw(logo, ww/2 - logo:getWidth()/2, 100)
-  for i, button in ipairs(buttons) do
-    button:draw()
-  end
-end
-
-function love.mousepressed(x, y, button, isTouch)
-  for i, button in ipairs(buttons) do
-    button:mousepressed(x, y, button, isTouch)
-  end
+    Gamestate.registerEvents()
+    Gamestate.switch(menu)
 end
