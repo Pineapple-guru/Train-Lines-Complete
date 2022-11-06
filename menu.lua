608e67e18aa058a0a258f98c384f60047cf81040
mwasanje <noaheben@gmail.com>
2022-11-05 22:09:44 -0500

    Created continuous scrolling screen.

--- menu.lua
+++ menu.lua
@@ -1,12 +1,19 @@
 menu = {}
 
 buttons = {}
+cloudTable= {}
+cloudTable.x1 = 0
+cloudTable.x2 = 0 - love.graphics.getWidth()
+cloudTable.x3 = 0
+
 
 function menu:init()
   ww = love.graphics.getWidth()
   wh = love.graphics.getHeight()
   button = love.graphics.newImage('images/button1.png')
   bg = love.graphics.newImage('images/bg.png')
+  cloud1 = love.graphics.newImage('images/clouds2.png')
+  cloud2 = love.graphics.newImage('images/clouds2.png')
   logo = love.graphics.newImage('images/logo.png')
   button_1 = Button(ww/2 - button:getWidth()/2, 300, "Play")
   table.insert(buttons, button_1)
@@ -15,8 +22,14 @@ function menu:init()
 end
 
 function menu:draw()
-  love.graphics.draw(bg, 0, 0)
-  love.graphics.draw(logo, ww/2 - logo:getWidth()/2, 100)
+  -- love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
+  love.graphics.draw(cloud1, cloudTable.x3,0)
+  love.graphics.draw(cloud1, cloudTable.x1,0)
+  love.graphics.draw(cloud2, cloudTable.x2,0)
+  -- love.graphics.draw(logo, ww/2 - logo:getWidth()/2, 100)
+  love.graphics.setFont(font2)
+  love.graphics.print("Train Lines", ww/2 - logo:getWidth()/2, 100)
+  love.graphics.setFont(font3)
   for i, button in ipairs(buttons) do
     button:draw()
   end
@@ -29,6 +42,17 @@ function menu:mousepressed(x, y, button, isTouch)
 end
 
 function menu:update(dt)
+  cloudTable.x1 = cloudTable.x1 + 1
+  cloudTable.x2 = cloudTable.x2 + 1
+
+  if cloudTable.x1 > love.graphics.getWidth() then
+    cloudTable.x1 = 0 - love.graphics.getWidth()
+  end
+
+  if cloudTable.x2 > love.graphics.getWidth() then
+    cloudTable.x2 = 0 - love.graphics.getWidth()
+  end
+
   for i, button in ipairs(buttons) do
     button:update(dt)
   end
