1b050741722dbba7381229513a73987cf8c4a831
mwasanje <noaheben@gmail.com>
2022-11-05 22:13:52 -0500

    Added and changed images used.

--- levelOne.lua
+++ levelOne.lua
@@ -21,7 +21,7 @@ function levelOne:init ()
           table.insert(object_tracker[i + 1], empty_tile)
       end
   end
-  local start_tile = Tile(2 * x, 5 * y, "start", "images/start_tile.png")
+  local start_tile = Tile(2 * x, 5 * y, "start", "images/TrainStation3.png")
   object_tracker[3][6] = start_tile
   local end_tile = Tile(12 * x, 1 * y, "end", "images/end_tile.png")
   object_tracker[13][2] = end_tile
@@ -45,7 +45,7 @@ function levelOne:update (dt)
   end
   if love.mouse.isDown(1) and track_limit > 0 then
       if ((mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] - 1 and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2]) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] + 1 and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2]) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2] - 1) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2] + 1)) and check_square(mouse_floor_x, mouse_floor_y) then
-          local track = Tile(mouse_floor_x * x, mouse_floor_y * y, "track", "images/track_tile.png")
+          local track = Tile(mouse_floor_x * x, mouse_floor_y * y, "track", "images/trackHorizontal.png")
           object_tracker[mouse_floor_x + 1][mouse_floor_y + 1] = track
           table.insert(current_tile_stack, {mouse_floor_x, mouse_floor_y})
           track_limit = track_limit - 1
@@ -53,7 +53,7 @@ function levelOne:update (dt)
   end
   if love.mouse.isDown(2) then
       if mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2] and object_tracker[mouse_floor_x + 1][mouse_floor_y + 1].name == "track" then
-          local empty_tile = Tile(mouse_floor_x * x, mouse_floor_y * y, "empty", "empty_tile.png")
+          local empty_tile = Tile(mouse_floor_x * x, mouse_floor_y * y, "empty", "images/empty_tile.png")
           object_tracker[mouse_floor_x + 1][mouse_floor_y + 1] = empty_tile
           table.remove(current_tile_stack, table.maxn(current_tile_stack))
           track_limit = track_limit + 1
@@ -68,9 +68,12 @@ function levelOne:draw ()
           tile:draw()
       end
   end
-  love.graphics.print("Number of tracks left: " .. track_limit, 0, 0)
+  love.graphics.setFont(font4)
+  love.graphics.print("Number of tracks left: " .. track_limit, 16, 10)
   if success then
-      love.graphics.print("Congratulations, you beat the level!", 524, 0)
+    -- love.graphics.draw()
+    love.graphics.setFont(font2)
+    love.graphics.print("Good Job!", love.graphics.getWidth()/4 + 64, love.graphics.getHeight()/2 + 96)
   end
 end
 
