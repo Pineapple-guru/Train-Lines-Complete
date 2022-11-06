7e6931273bc51daaedb5430bbbd637bcfda2f6c4
mwasanje <noaheben@gmail.com>
2022-11-05 22:15:24 -0500

    Added different font sizes.

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
+love.graphics.setDefaultFilter('nearest','nearest')
 font1 = love.graphics.newFont("fonts/monogram.ttf", 32)
-love.graphics.setFont(font1)
+font2 = love.graphics.newFont("fonts/monogram.ttf", 128)
+font3 = love.graphics.newFont("fonts/monogram.ttf", 64)
+font4 = love.graphics.newFont("fonts/monogram.ttf", 40)
+love.graphics.setFont(font3)
 Object = require "libraries/classic"
 Gamestate = require "libraries/gamestate"
 require "button"
